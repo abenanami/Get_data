fbeec19115c5c2c21151797d5a1f1de65347bcc9
abenanami <B21P002@akita-pu.ac.jp>
2022-10-11 17:50:14 +0900

    first commit

---
+++ app/src/main/java/com/example/getdata/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.example.getdata
+
+//import android.R
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import java.io.*
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private val fileName = "data.txt"
+
+    private var sensorManager: SensorManager? = null
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get an instance of the SensorManager
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Get an instance of the TextView
+        textView = findViewById(R.id.text_view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //ボタン
+        val textMessage = findViewById<TextView>(R.id.message)
+        val startbutton = findViewById<Button>(R.id.start)
+
+        //記録中
+        startbutton.setOnClickListener {
+            textMessage.setText(R.string.message_start)
+            // Listenerの登録
+            val accel = sensorManager!!.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER
+            )
+            sensorManager!!.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL)
+            //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+            //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME);
+            //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //ボタン
+        val textMessage = findViewById<TextView>(R.id.message)
+        val stopbutton = findViewById<Button>(R.id.stop)
+
+        //記録停止中
+        stopbutton.setOnClickListener {
+            textMessage.setText(R.string.message_stop)
+            // Listenerを解除
+            sensorManager!!.unregisterListener(this)
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SdCardPath")
+    override fun onSensorChanged(event: SensorEvent) {
+
+        val sensorX: Float
+        val sensorY: Float
+        val sensorZ: Float
+
+        //ボタン
+        val textMessage = findViewById<TextView>(R.id.message)
+        val resetbutton = findViewById<Button>(R.id.reset)
+        val buttonRead: Button = findViewById(R.id.data)
+        val stopbutton = findViewById<Button>(R.id.stop)
+
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            sensorX = event.values[0]
+            sensorY = event.values[1]
+            sensorZ = event.values[2]
+            val strTmp = "${getNowDate(event)} X: $sensorX Y: $sensorY Z: $sensorZ,"
+            textView!!.text = strTmp
+            savefile(strTmp)
+        }
+
+        //記録停止中
+        stopbutton.setOnClickListener {
+            textMessage.setText(R.string.message_stop)
+            // Listenerを解除
+            sensorManager!!.unregisterListener(this)
+        }
+
+        //ファイルを削除
+        resetbutton.setOnClickListener {
+            val path = Paths.get("/data/data/com.example.getdata/files/data.txt")
+
+            try {
+                Files.deleteIfExists(path)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        //読み出し
+        buttonRead.setOnClickListener {
+            val str = readFile()
+            if (str != null) {
+                textMessage.text = str
+            } else {
+                textMessage.setText(R.string.read_error)
+            }
+        }
+    }
+
+    // ファイルを保存
+    private fun savefile(str: String) {
+
+        try {
+            openFileOutput( fileName, Context.MODE_APPEND).use {
+                    fileOutputstream -> fileOutputstream.write(str.toByteArray()) }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    // ファイルを読み出し
+    private fun readFile(): String? {
+        var text: String? = null
+
+        try {
+            openFileInput(fileName).use { fileInputStream ->
+                BufferedReader(
+                    InputStreamReader(fileInputStream, StandardCharsets.UTF_8)
+                ).use { reader ->
+                    var lineBuffer: String?
+                    while (reader.readLine().also { lineBuffer = it } != null) {
+                        text = lineBuffer
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return text
+    }
+
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    companion object {
+        private fun getNowDate(event: SensorEvent): String {
+            val df: DateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+            df.timeZone = TimeZone.getTimeZone("Asia/Tokyo")
+            val date = Date(System.currentTimeMillis())
+            return df.format(date)
+        }
+    }
+}
